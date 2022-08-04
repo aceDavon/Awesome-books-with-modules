2fd9f1516cc4c56fb0624c7fd6f048c1f4e420f9
michael-duke <michael.21duke@gmail.com>
2022-08-04 03:15:43 +0300

    Make 'Qken' or 'Date in Amharic' class, add Time class and set interval to display current time

--- js/main.js
+++ js/main.js
@@ -9,7 +9,8 @@ class DynamicBook {
     bookInfoContainer.classList.add('p-4');
 
     const message = document.createElement('span');
-    message.innerText = 'Your book collection is empty. Kindly add your favorite books.';
+    message.innerText =
+      'Your book collection is empty. Kindly add your favorite books.';
     message.className = 'empty-message italic';
 
     const smileyFace = document.createElement('span');
@@ -26,10 +27,12 @@ class DynamicBook {
 
     const sectionTitle = document.createElement('h2');
     sectionTitle.innerText = 'All awesome books';
-    sectionTitle.className = 'text-slate-50 text-center self-center font-bold text-3xl';
+    sectionTitle.className =
+      'text-slate-50 text-center self-center font-bold text-3xl';
 
     const bookInfoContainer = document.createElement('div');
-    bookInfoContainer.className = 'book-info-container bg-blue-100 pb-1 max-h-64 overflow-y-scroll rounded border-0 shadow-lg shadow-slate-500/50';
+    bookInfoContainer.className =
+      'book-info-container bg-blue-100 pb-1 max-h-64 overflow-y-scroll rounded border-0 shadow-lg shadow-slate-500/50';
     bookDisplay.append(sectionTitle, bookInfoContainer);
 
     return bookDisplay;
@@ -112,7 +115,8 @@ class BooksCollection {
       remove`;
       removeBtn.setAttribute('data-mdb-ripple', 'true');
       removeBtn.setAttribute('data-mdb-ripple-color', 'light');
-      removeBtn.classList = 'primary-btn del-btn flex flex-row px-2 pt-1 mr-2 pb-0.5 inline-block rounded text-red-500 leading-normal capitalize border border-red-300 shadow-md hover:bg-red-500 hover:text-white hover:shadow-lg hover:shadow-red-500/50 active:bg-red-700 active:shadow-lg transition duration-150 ease-in-out w-fit h-fit';
+      removeBtn.classList =
+        'primary-btn del-btn flex flex-row px-2 pt-1 mr-2 pb-0.5 inline-block rounded text-red-500 leading-normal capitalize border border-red-300 shadow-md hover:bg-red-500 hover:text-white hover:shadow-lg hover:shadow-red-500/50 active:bg-red-700 active:shadow-lg transition duration-150 ease-in-out w-fit h-fit';
       removeBtn.onclick = () => this.onDelete(id);
       removeBtn.addEventListener('mouseover', () => {
         bookInfo.classList.add('line-through');
@@ -150,3 +154,61 @@ DynamicBook.isCollectionEmpty();
 if (!messageOn) BooksCollection.renderBooks(bookCollection);
 const addBtn = document.querySelector('.add-btn');
 addBtn.onclick = () => BooksCollection.addBook();
+
+class Qken {
+  constructor() {
+    this.date = new Date();
+    this.months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+  }
+
+  getDate() {
+    let day;
+    if (this.date.getDate() < 4) {
+      if (this.lastDigit(this.date.getDate) === 1)
+        day = this.date.getDate() + 'st';
+      else if (this.lastDigit(this.date.getDate) === 2)
+        day = this.date.getDate() + 'nd';
+      else if (this.lastDigit(this.date.getDate) === 3)
+        day = this.date.getDate() + 'rd';
+    } else day = this.date.getDate() + 'th';
+
+    document.getElementById('date').innerHTML = `${
+      this.months[this.date.getMonth()]
+    } ${day} ${this.date.getFullYear()}, `;
+  }
+
+  static lastDigit(number) {
+    return +number.toString().slice(-1);
+  }
+}
+
+class Time {
+  constructor() {}
+
+  static displayTime() {
+    const time = new Date();
+    const hrs = time.getHours();
+    const mins = time.getMinutes();
+    const secs = time.getSeconds();
+
+    document.getElementById('hours').innerHTML = hrs + ':';
+    document.getElementById('minutes').innerHTML = mins < 10 ? `0${mins}:`: mins+':';
+    document.getElementById('seconds').innerHTML = secs <10 ? `0${secs}`: secs;
+  }
+}
+const qken = new Qken();
+qken.getDate();
+setInterval(Time.displayTime, 10);
